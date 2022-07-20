85f541fa68ccf4557152254c3865a57ea14f3017
Ethan - 4cqker <4cqker@gmail.com>
2022-07-20 21:27:23 +0930

    Added Gun to the player in the playground scene

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode fireKey = KeyCode.Mouse0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(fireKey)) GunShoot();
+    }
+
+    private void GunShoot()
+    {
+        Debug.Log("Bang!");
+    }
+}
