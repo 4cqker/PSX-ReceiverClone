b3d2ce5054eec3330ce7db7b25d969247dd602fc
RhysNortonMK <rhys.norton@mightykingdom.com>
2022-07-20 22:39:47 +0930

    Gun Ray and Enemy Patrol
    -Gun Shoots a ray and can hit targets
    -Enemy that patrols

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform[] movePositions;
+    [SerializeField] private float minDistance = 0.1f;
+
+    private NavMeshAgent agent;
+    private Transform moveTarget;
+
+    private void Start()
+    {
+        if (!TryGetComponent(out agent)) Debug.LogError("No agent on the enemy!");
+
+        SetMoveTarget(movePositions[Random.Range(0, movePositions.Length)]);
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, moveTarget.position) < minDistance)
+        {
+            Debug.Log("Change target");
+            SetMoveTarget(movePositions[Random.Range(0, movePositions.Length)]);
+        }
+    }
+
+    private void SetMoveTarget(Transform transform)
+    {
+        moveTarget = transform;
+        agent.SetDestination(moveTarget.position);
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private KeyCode fireKey = KeyCode.Mouse0;
+    [SerializeField] private Transform bulletOrigin;
+    [SerializeField] private LayerMask hitMask;
+    [SerializeField] private string enemyLayer;
+    [SerializeField] private string objectLayer;
+
+    private void Start()
+    {
+        if (!bulletOrigin) bulletOrigin = transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(fireKey)) GunShoot();
+    }
+
+    private void GunShoot()
+    {
+        Debug.Log("Bang!");
+
+        if (Physics.Raycast(bulletOrigin.position, bulletOrigin.forward, out RaycastHit hitInfo, Mathf.Infinity, hitMask))
+        {
+            Debug.Log(hitInfo.collider.name);
+
+            if (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer(enemyLayer))
+            {
+                Destroy(hitInfo.collider.gameObject);
+                Debug.Log("Hit target!");
+            }
+            else if (hitInfo.collider.gameObject.layer == LayerMask.NameToLayer(objectLayer))
+            {
+                Debug.Log("Hit the ground!");
+            }
+            else
+            {
+                //idk
+            }
+        }
+    }
+}
