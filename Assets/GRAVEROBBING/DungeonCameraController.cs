7f1b7223922df7f938a6ad123b4b245b0b70f688
4cqker <4cqker@gmail.com>
2023-04-08 22:38:41 +0930

    Started working on a custom cam controller for a Graverobber Game

---
+++ Assets/GRAVEROBBING/DungeonCameraController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class DungeonCameraController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float jumpHeight = 2f;
+    public float crouchHeight = 0.5f;
+    public float mouseSensitivity = 100f;
+    public float minimumVerticalAngle = -85.0f;
+    public float maximumVerticalAngle = 85.0f;
+
+    private CharacterController controller;
+
+    private float verticalAngle = 0.0f;
+    private float horizontalAngle = 0.0f;
+
+    private bool isCrouching = false;
+    private float defaultHeight;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        defaultHeight = controller.height;
+    }
+
+    void Update()
+    {
+        
+        if (controller.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                //jump
+            }
+
+            if (Input.GetButtonDown("Crouch"))
+            {
+                //crouch
+            }
+        }
+
+        //WASD Movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movementVector = new Vector3(horizontalInput, 0, verticalInput) * movementSpeed * Time.deltaTime;
+        controller.Move(movementVector);
+
+        //Mouse Looking
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        horizontalAngle += mouseX * mouseSensitivity;
+        verticalAngle -= mouseY * mouseSensitivity;
+        verticalAngle = Mathf.Clamp(verticalAngle, minimumVerticalAngle, maximumVerticalAngle);
+
+        transform.localRotation = Quaternion.Euler(verticalAngle, horizontalAngle, 0.0f);
+
+    }
+}
