62a2675a7d1ef2a69582de9a1dd2a37922795b6c
4cqker <4cqker@gmail.com>
2023-04-09 00:28:51 +0930

    Added Headbobbing and lots of refinement to the controller's camera.

--- Assets/GRAVEROBBING/DungeonCameraController.cs
+++ Assets/GRAVEROBBING/DungeonCameraController.cs
@@ -3,47 +3,68 @@
 public class DungeonCameraController : MonoBehaviour
 {
     public float movementSpeed = 5f;
-    public float jumpHeight = 2f;
+    
     public float crouchHeight = 0.5f;
     public float mouseSensitivity = 100f;
-    public float minimumVerticalAngle = -85.0f;
-    public float maximumVerticalAngle = 85.0f;
+    public float minimumVerticalAngle = -80.0f;
+    public float maximumVerticalAngle = 80.0f;
+
+    public float headBobModifier = 5f;
+    public float headBobAmplifier = 1.3f;
+    public float headBobSpeed = 1.3f;
+    public float bobResetSpeed = 0.2f;
+    public float grav = -4f;
+    public float jumpForce = 2f;
 
     private CharacterController controller;
 
     private float verticalAngle = 0.0f;
     private float horizontalAngle = 0.0f;
+    private Vector3 originalPosition;
 
     private bool isCrouching = false;
     private float defaultHeight;
+    private float verticalVelocity = 0f;
+    private float airTimer = 0f;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
         defaultHeight = controller.height;
+        originalPosition = Camera.main.transform.localPosition;
     }
 
     void Update()
     {
-        
         if (controller.isGrounded)
         {
+            airTimer = 0f;
             if (Input.GetButtonDown("Jump"))
             {
-                //jump
+                //Jump
             }
+        }
+        else
+        {
+            airTimer += 0.1f;
+        }
 
-            if (Input.GetButtonDown("Crouch"))
-            {
-                //crouch
-            }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            //crouch
         }
 
         //WASD Movement
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movementVector = new Vector3(horizontalInput, 0, verticalInput) * movementSpeed * Time.deltaTime;
-        controller.Move(movementVector);
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 forward = transform.forward;
+        forward.y = 0f;
+        forward = forward.normalized;
+        Vector3 moveDirection = forward * verticalInput + transform.right * horizontalInput;
+        moveDirection = moveDirection.normalized * movementSpeed * Time.deltaTime;
+        controller.Move(moveDirection);
 
         //Mouse Looking
         float mouseX = Input.GetAxis("Mouse X");
@@ -52,8 +73,29 @@ public class DungeonCameraController : MonoBehaviour
         horizontalAngle += mouseX * mouseSensitivity;
         verticalAngle -= mouseY * mouseSensitivity;
         verticalAngle = Mathf.Clamp(verticalAngle, minimumVerticalAngle, maximumVerticalAngle);
+        transform.localRotation = Quaternion.Euler(verticalAngle, horizontalAngle, transform.localRotation.z);
 
-        transform.localRotation = Quaternion.Euler(verticalAngle, horizontalAngle, 0.0f);
+        //Headbobbing
+        HeadBobbing(headBobSpeed, headBobAmplifier, moveDirection.magnitude * headBobModifier, Camera.main.transform);
+        if (moveDirection.magnitude < 0.0001f)
+        {
+            //Camera.main.transform.localPosition = Vector3.Lerp(Camera.main.transform.localPosition, originalPosition, Time.deltaTime * bobResetSpeed);
 
+
+            Vector3 currentVelocity = Vector3.zero;
+            Camera.main.transform.localPosition = Vector3.SmoothDamp(Camera.main.transform.localPosition, originalPosition, ref currentVelocity, bobResetSpeed);
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        controller.Move(new Vector3(0, grav * airTimer * Time.deltaTime, 0));
+    }
+
+    void HeadBobbing(float bobbingFrequency, float bobbingAmplitude, float movementSpeed, Transform cam)
+    {
+        float bobbingAmount = Mathf.Sin(Time.time * 2 * Mathf.PI * bobbingFrequency) * bobbingAmplitude;
+        Vector3 newPosition = new Vector3(originalPosition.x, originalPosition.y + bobbingAmount, originalPosition.z);
+        cam.localPosition = Vector3.Lerp(cam.localPosition, newPosition, Time.deltaTime * movementSpeed);
     }
 }
