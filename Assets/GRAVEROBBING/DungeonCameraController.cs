b894cf125d270923cd299fd70244246b0a1659c1
RhysNortonMK <rhys.norton@mightykingdom.com>
2023-04-10 23:30:04 +0930

    Char Controller Cleanup
    -Variable name changes
    -Added headers and spaces
    -Fixed head bobbing to not use lerping
    -Fixed camera tilt by using lerping instead of smoothdamp

--- Assets/GRAVEROBBING/DungeonCameraController.cs
+++ Assets/GRAVEROBBING/DungeonCameraController.cs
@@ -3,18 +3,23 @@
 public class DungeonCameraController : MonoBehaviour
 {
     public float movementSpeed = 5f;
-    
+    [Space]
     public float crouchHeight = 0.5f;
+    [Space]
     public float mouseSensitivity = 100f;
-    public float minimumVerticalAngle = -80.0f;
-    public float maximumVerticalAngle = 80.0f;
-
-    public float headBobModifier = 5f;
-    public float headBobAmplifier = 1.3f;
-    public float headBobSpeed = 1.3f;
-    public float bobResetSpeed = 0.2f;
+    public float minVerticalLookAngle = -80.0f;
+    public float maxVerticalLookAngle = 80.0f;
+    [Space]
     public float grav = -4f;
     public float jumpForce = 2f;
+    [Space]
+    [Header("Game Feel")]
+    public float headBobAmplitude = 1.3f;
+    public float headBobFrequency = 1.3f;
+    private float headBobAmount = 0f;
+    [Space]
+    public float cameraTiltAngle = 15f;
+    public float cameraTiltSpeed = 0.1f;
 
     private CharacterController controller;
 
@@ -35,6 +40,9 @@ public class DungeonCameraController : MonoBehaviour
         defaultHeight = controller.height;
         originalPosition = Camera.main.transform.localPosition;
         rollHandler = Camera.main.transform.parent.gameObject;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
@@ -57,58 +65,60 @@ public class DungeonCameraController : MonoBehaviour
             //crouch
         }
 
-        //WASD Movement
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        Movement();
 
+        MouseLooking();
 
-        Vector3 forward = transform.forward;
-        forward.y = 0f;
-        forward = forward.normalized;
-        Vector3 moveDirection = forward * verticalInput + transform.right * horizontalInput;
-        moveDirection = moveDirection.normalized * movementSpeed * Time.deltaTime;
-        controller.Move(moveDirection);
+        HeadBobbing();
 
-        //Mouse Looking
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        CameraRoll();
 
-        horizontalAngle += mouseX * mouseSensitivity;
-        verticalAngle -= mouseY * mouseSensitivity;
-        verticalAngle = Mathf.Clamp(verticalAngle, minimumVerticalAngle, maximumVerticalAngle);
-        transform.localRotation = Quaternion.Euler(verticalAngle, horizontalAngle, transform.localRotation.z);
-
-        //Headbobbing
-        HeadBobbing(headBobSpeed, headBobAmplifier, moveDirection.magnitude * headBobModifier, Camera.main.transform);
-        if (moveDirection.magnitude < 0.0001f)
+        void Movement()
         {
-            //Camera.main.transform.localPosition = Vector3.Lerp(Camera.main.transform.localPosition, originalPosition, Time.deltaTime * bobResetSpeed);
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
 
 
-            Vector3 currentVelocity = Vector3.zero;
-            Camera.main.transform.localPosition = Vector3.SmoothDamp(Camera.main.transform.localPosition, originalPosition, ref currentVelocity, bobResetSpeed);
+            Vector3 forward = transform.forward;
+            forward.y = 0f;
+            forward = forward.normalized;
+            Vector3 moveDirection = forward * verticalInput + transform.right * horizontalInput;
+            moveDirection = moveDirection.normalized * movementSpeed * Time.deltaTime;
+            controller.Move(moveDirection);
         }
-        //Camera Tilting is broken when turning Left. Bug fix if you want the feature in @RHYS
-        //UpdateCameraRoll(rollHandler, 6f, 0.2f);
 
+        void MouseLooking()
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            horizontalAngle += mouseX * mouseSensitivity;
+            verticalAngle -= mouseY * mouseSensitivity;
+            verticalAngle = Mathf.Clamp(verticalAngle, minVerticalLookAngle, maxVerticalLookAngle);
+            transform.localRotation = Quaternion.Euler(verticalAngle, horizontalAngle, transform.localRotation.z);
+        }
+
+        void HeadBobbing()
+        {
+            if (controller.isGrounded || !Mathf.Approximately(0f, headBobAmount))
+            headBobAmount = Mathf.Sin(Time.time * headBobFrequency) * headBobAmplitude;
+            Camera.main.transform.localPosition = new Vector3(originalPosition.x, originalPosition.y + headBobAmount, originalPosition.z);
+        }
+
+        void CameraRoll()
+        {
+            Quaternion targetRoll = Quaternion.Euler(new Vector3(
+                rollHandler.transform.eulerAngles.x,
+                rollHandler.transform.eulerAngles.y,
+                Input.GetAxisRaw("Horizontal") * -cameraTiltAngle));
+
+            rollHandler.transform.rotation = Quaternion.Lerp(rollHandler.transform.rotation, targetRoll, Time.deltaTime / cameraTiltSpeed);
+        }
     }
-    
+
     private void FixedUpdate()
     {
+        //todo I don't think this is working properly
         controller.Move(new Vector3(0, grav * airTimer * Time.deltaTime, 0));
-    }
-
-    void HeadBobbing(float bobbingFrequency, float bobbingAmplitude, float movementSpeed, Transform cam)
-    {
-        float bobbingAmount = Mathf.Sin(Time.time * 2 * Mathf.PI * bobbingFrequency) * bobbingAmplitude;
-        Vector3 newPosition = new Vector3(originalPosition.x, originalPosition.y + bobbingAmount, originalPosition.z);
-        cam.localPosition = Vector3.Lerp(cam.localPosition, newPosition, Time.deltaTime * movementSpeed);
-    }
-
-    void UpdateCameraRoll(GameObject rollHandler, float maxRollAngle, float smoothTime)
-    {
-        float targetRoll = Mathf.Clamp(Input.GetAxis("Mouse X") * 2f, -maxRollAngle, maxRollAngle);
-        float currentRoll = Mathf.SmoothDamp(rollHandler.transform.eulerAngles.z, targetRoll, ref rollVelocity, smoothTime);
-        rollHandler.transform.eulerAngles = new Vector3(rollHandler.transform.eulerAngles.x, rollHandler.transform.eulerAngles.y, currentRoll);
     }
 }
